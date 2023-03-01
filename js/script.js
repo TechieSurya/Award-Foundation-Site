59d50ee7ebf604f4966022a2a9a78d077b43ffcd
TechieSurya <104114616+TechieSurya@users.noreply.github.com>
2023-03-01 11:00:24 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -8,3 +8,17 @@ videoBtn.forEach(btn =>{
         document.querySelector('#video-slider').src=src;
     });
 });
+
+window.addEventListener("load", function(){
+  setTimeout(
+    function open(event){
+        document.querySelector(".popup").style.display="block";
+    },
+    1000
+  )
+});
+
+document.querySelector("#close").addEventListener("click", function(){
+    document.querySelector(".popup").
+    style.display="none";
+});
